082da75c5ec08f1905cf1c0b1e08e94bf435b221
unknown <jarulcrip@gmail.com>
2025-11-07 18:33:59 +0200

    ready adaptive and modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,20 @@
-// Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
-
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
-});
-
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+// Модальное окно
+
+const modal = document.querySelector('.modal');
+const openModalBtn = document.querySelector("[data-action='modal']");
+const closeModalBtn = document.querySelector('#closeModal');
+const modalBody = document.querySelector('.modal__body');
+
+openModalBtn.addEventListener('click', function() {
+	modal.classList.add('modal--open');
+	document.body.classList.add('no-scroll');
+});
+
+closeModalBtn.addEventListener('click', function() {
+	modal.classList.remove('modal--open');
+	document.body.classList.remove('no-scroll');
+});
